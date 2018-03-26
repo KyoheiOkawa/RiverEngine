c39c60a4321b4a3c2f1c5ff42e528a75e8e662c1
KyoheiOkawa <kurokenken0310@gmail.com>
2018-03-27 01:24:16 +0900

    Fbx変換プロジェクトの作成

---
+++ FbxConverter/FbxConverter/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  FbxConverter
+//
+//  Created by 大川恭平 on 2018/03/27.
+//  Copyright © 2018年 KyoheiOkawa. All rights reserved.
+//
+
+#include <iostream>
+#include <fbxsdk.h>
+
+int main(int argc, const char * argv[])
+{
+    
+    
+    return 0;
+}
